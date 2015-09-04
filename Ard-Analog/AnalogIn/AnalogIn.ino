91e92761455d509bad933099e46635a2c435bcf1
CurtisIreland <pi@raspberrypi.(none)>
2015-09-03 23:40:20 -0400

    Add Arduino program for working with Analog data

---
+++ Ard-Analog/AnalogIn/AnalogIn.ino
@@ -0,0 +1,22 @@
+const int analogInPin = A0;
+const int analogOutPin = 3;
+
+int sensorValue = 0;
+int outputValue = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorValue = analogRead(analogInPin);
+  outputValue = map(sensorValue, 0, 1023, 0, 255);
+  analogWrite(analogOutPin, outputValue);
+
+  // print the results to the serial monitor:
+  Serial.print("sensor = " );
+  Serial.print(sensorValue);
+  Serial.print("\t output = ");
+  Serial.println(outputValue);
+  delay(250);
+}
