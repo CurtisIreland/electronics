0168b98f7bbbdf96926c61500148c1a40896268c
CurtisIreland <pi@raspberrypi.(none)>
2015-10-26 23:39:36 -0400

    Using the darlington pair as a touch sensor for the Arduino

---
+++ Ard-TouchSensor/Touch1/Touch1.ino
@@ -0,0 +1,16 @@
+const int analogInPin = A1;
+
+int sensorValue = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorValue = analogRead(analogInPin);
+
+  // print the results to the serial monitor:
+  Serial.print("sensor = " );
+  Serial.println(sensorValue);
+  delay(250);
+}
