6cda4711a7d93f87a6e3d3b56274cb01cf5f10eb
CurtisIreland <pi@raspberrypi.(none)>
2016-03-01 20:08:35 -0500

    Arduino for loops and arrays

---
+++ Ard-Programming/ForInterrupt/ForInterrupt.ino
@@ -0,0 +1,30 @@
+int led_array[] = { 10, 11, 12, 13 };
+int led_count = 4;
+volatile int led_pos = 0;
+
+int button_pin = 2;
+
+void setup() {
+  for (int this_led = 0; this_led < led_count; this_led++) {
+    pinMode(led_array[this_led], OUTPUT);
+    digitalWrite(led_array[this_led], LOW);
+  }
+
+  pinMode(button_pin, INPUT);
+  attachInterrupt(0, change, FALLING);
+}
+
+void loop() {
+  for (int this_led = 0; this_led < led_count; this_led++) {
+    if( this_led == led_pos ) {
+      digitalWrite(led_array[this_led], HIGH);
+    } else {
+      digitalWrite(led_array[this_led], LOW);
+    }
+  }
+}
+
+void change() {
+  led_pos = (led_pos + 1) % 4;
+}
+
