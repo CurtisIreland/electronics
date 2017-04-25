81d001cfbf2dbf5344c5916095d015a0ef4f0513
Curtis Ireland <cireland@in-works.net>
2017-04-24 23:38:57 -0400

    Support files for Electronics - MOSFETs video

---
+++ Electronics-MOSFET/MOSFET-Var/MOSFET-Var.ino
@@ -0,0 +1,23 @@
+const int analogInPin = A0;
+const int mosGatePin = 3;
+
+int inValue = 0;
+int outValue = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(mosGatePin, OUTPUT);
+}
+
+void loop() {
+  inValue = analogRead(analogInPin);
+  outValue = map(inValue, 0, 1023, 0, 255);
+  analogWrite(mosGatePin, outValue);
+
+  // print the results to the serial monitor:
+  Serial.print("input = " );
+  Serial.print(inValue);
+  Serial.print("\t MOSFET = ");
+  Serial.println(outValue);
+  delay(250);
+}
---
+++ Electronics-MOSFET/MOSFET/MOSFET.ino
@@ -0,0 +1,15 @@
+const int nMOS = 3;
+
+void setup() {
+  pinMode(nMOS, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(nMOS, HIGH);
+  digitalWrite(13, HIGH);
+  delay(750);
+  digitalWrite(nMOS, LOW);
+  digitalWrite(13, LOW);
+  delay(750);
+}
