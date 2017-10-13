f248e0ac9dcfb6e43b58fb3ad24dc16bcd39f762
Luis F Cevallos <luisfcevallos@gmail.com>
2017-10-13 11:39:45 -0700

    Update ButtonLED3.ino

--- Ard-Inputs/ButtonLED3/ButtonLED3.ino
+++ Ard-Inputs/ButtonLED3/ButtonLED3.ino
@@ -3,34 +3,35 @@
 #define BLUE_LED 10
 #define BUTTON_PIN 2
 
-int interval=200; // the time we need to wait
-unsigned long previousMillis=0; // millis() returns an unsigned long.
+int interval=200;
+unsigned long previousMillis=0;
+unsigned long currentMillis=0;
 
 int choose_led = 0;
 
 void change_led() {
-  unsigned long currentMillis = millis();
+  currentMillis = millis();
   if (currentMillis - previousMillis >= interval){
-  if (choose_led == 0) {
-    digitalWrite(RED_LED, HIGH);
-    digitalWrite(GREEN_LED, LOW);
-    digitalWrite(BLUE_LED, LOW);
-    Serial.println("RED");
-  } else if (choose_led == 1) {
-    digitalWrite(RED_LED, LOW);
-    digitalWrite(GREEN_LED, HIGH);
-    digitalWrite(BLUE_LED, LOW);
-    Serial.println("GREEN");
-  } else if (choose_led == 2) {
-    digitalWrite(RED_LED, LOW);
-    digitalWrite(GREEN_LED, LOW);
-    digitalWrite(BLUE_LED, HIGH);
-    Serial.println("BLUE");
+    if (choose_led == 0) {
+      digitalWrite(RED_LED, HIGH);
+      digitalWrite(GREEN_LED, LOW);
+      digitalWrite(BLUE_LED, LOW);
+      Serial.println("RED");
+    } else if (choose_led == 1) {
+      digitalWrite(RED_LED, LOW);
+      digitalWrite(GREEN_LED, HIGH);
+      digitalWrite(BLUE_LED, LOW);
+      Serial.println("GREEN");
+    } else if (choose_led == 2) {
+      digitalWrite(RED_LED, LOW);
+      digitalWrite(GREEN_LED, LOW);
+      digitalWrite(BLUE_LED, HIGH);
+      Serial.println("BLUE");
+    }
+
+    choose_led = (choose_led + 1) % 3;
+    previousMillis = millis();
   }
-  
-  choose_led = (choose_led + 1) % 3;
-  previousMillis = millis();
-}
 }
 
 void setup() {
