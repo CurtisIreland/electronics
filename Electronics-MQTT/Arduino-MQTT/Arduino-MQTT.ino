96552115014b8951a3a3b3efddda88e5b3bfb868
Curtis Ireland <cireland@in-works.net>
2017-12-11 22:48:24 -0500

    Files for MQTT Video

---
+++ Electronics-MQTT/Arduino-MQTT/Arduino-MQTT.ino
@@ -0,0 +1,59 @@
+#include <SPI.h>
+#include <Servo.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+#define SERVO_CONTROL 6
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+Servo servo;
+EthernetClient ethClient;
+// mqtt configuration
+const char* server = "192.168.5.10";
+const char* topic = "example/pot";
+const char* clientName = "com.be.arduino";
+PubSubClient client(ethClient);
+void callback(char* topic, byte* payload, unsigned int length) {
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] angle:");
+    String data;
+    for (int i = 0; i < length; i++) {
+        data += (char)payload[i];
+    }
+    double angle = ((data.toInt() * 180) / 100);
+    constrain(angle, 0, 180);
+    servo.write((int) angle);
+    Serial.println((int) angle);
+}
+void mqttReConnect() {
+    while (!client.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        // Attempt to connect
+        if (client.connect(clientName)) {
+            Serial.println("connected");
+            client.subscribe(topic);
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            delay(5000);
+        }
+    }
+}
+void setup()
+{
+    Serial.begin(9600);
+    client.setServer(server, 1883);
+    client.setCallback(callback);
+    servo.attach(SERVO_CONTROL);
+    if (Ethernet.begin(mac) == 0) {
+        Serial.println("Failed to configure Ethernet using DHCP");
+    }
+    delay(1500); // Allow the hardware to sort itself out
+}
+void loop()
+{
+    if (!client.connected()) {
+        mqttReConnect();
+    }
+    client.loop();
+}
---
+++ Electronics-MQTT/NodeMCU-MQTT/NodeMCU-MQTT.ino
@@ -0,0 +1,79 @@
+#include <PubSubClient.h>
+#include <ESP8266WiFi.h>
+// Wifi configuration
+const char* ssid = "BasementElectronics";
+const char* password = "AmateurHour";
+// mqtt configuration
+const char* server = "192.168.5.10";
+const char* topic = "example/pot";
+const char* clientName = "com.be.nodemcu";
+int value;
+int percent;
+String payload;
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+void wifiConnect() {
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.print("WiFi connected.");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    if (client.connect(clientName)) {
+        Serial.print("Connected to MQTT broker at ");
+        Serial.print(server);
+        Serial.print(" as ");
+        Serial.println(clientName);
+        Serial.print("Topic is: ");
+        Serial.println(topic);
+    }
+    else {
+        Serial.println("MQTT connect failed");
+        Serial.println("Will reset and try again...");
+        abort();
+    }
+}
+void mqttReConnect() {
+    while (!client.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        // Attempt to connect
+        if (client.connect(clientName)) {
+            Serial.println("connected");
+            client.subscribe(topic);
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            delay(5000);
+        }
+    }
+}
+void setup() {
+    Serial.begin(9600);
+    client.setServer(server, 1883);
+    wifiConnect();
+    delay(10);
+}
+void loop() {
+    value = analogRead(A0);
+    percent = (int) ((value * 100) / 1010);
+    payload = (String) percent;
+    if (client.connected()) {
+        if (client.publish(topic, (char*) payload.c_str())) {
+            Serial.print("Publish ok (");
+            Serial.print(payload);
+            Serial.println(")");
+        } else {
+            Serial.println("Publish failed");
+        }
+    } else {
+        mqttReConnect();
+    }
+    delay(200);
+}
