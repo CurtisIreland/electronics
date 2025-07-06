5a765929498c7e70aadc53d6d41c7cd3c212a251
Curtis Ireland <curtis.ireland@gmail.com>
2025-07-06 19:35:23 -0400

    Python Scripts to use two SPI LCD displays on a Raspberry Pi

---
+++ RPi-2SPILCD/display_screens.py
@@ -0,0 +1,140 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+"""
+This demo will draw a few rectangles onto the screen along with some text
+on top of that.
+
+This example is for use on (Linux) computers that are using CPython with
+Adafruit Blinka to support CircuitPython libraries. CircuitPython does
+not support PIL/pillow (python imaging library)!
+
+Author(s): Melissa LeBlanc-Williams for Adafruit Industries
+"""
+
+import board
+import digitalio
+from PIL import Image, ImageDraw, ImageFont
+
+from adafruit_rgb_display import (
+    hx8357,
+    ili9341,
+    ssd1331,
+    ssd1351,
+    st7735,
+    st7789,
+)
+
+# First define some constants to allow easy resizing of shapes.
+BORDER = 20
+FONTSIZE = 24
+
+# Configuration for CS and DC pins (these are PiTFT defaults):
+cs_pin0 = digitalio.DigitalInOut(board.CE0)
+dc_pin0 = digitalio.DigitalInOut(board.D23)
+reset_pin0 = digitalio.DigitalInOut(board.D22)
+cs_pin1 = digitalio.DigitalInOut(board.CE1)
+dc_pin1 = digitalio.DigitalInOut(board.D25)
+reset_pin1 = digitalio.DigitalInOut(board.D24)
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 24000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the display:
+# disp = st7789.ST7789(spi, rotation=90,                            # 2.0" ST7789
+# disp = st7789.ST7789(spi, height=240, y_offset=80, rotation=180,  # 1.3", 1.54" ST7789
+# disp = st7789.ST7789(spi, rotation=90, width=135, height=240, x_offset=53, y_offset=40, # 1.14" ST7789
+# disp = st7789.ST7789(spi, rotation=90, width=172, height=320, x_offset=34, # 1.47" ST7789
+# disp = st7789.ST7789(spi, rotation=270, width=170, height=320, x_offset=35, # 1.9" ST7789
+# disp = hx8357.HX8357(spi, rotation=180,                           # 3.5" HX8357
+# disp = st7735.ST7735R(spi, rotation=90,                           # 1.8" ST7735R
+# disp = st7735.ST7735R(spi, rotation=270, height=128, x_offset=2, y_offset=3,   # 1.44" ST7735R
+# disp = st7735.ST7735R(spi, rotation=90, bgr=True, width=80,       # 0.96" MiniTFT Rev A ST7735R
+# disp = st7735.ST7735R(spi, rotation=90, invert=True, width=80,    # 0.96" MiniTFT Rev B ST7735R
+# x_offset=26, y_offset=1,
+# disp = ssd1351.SSD1351(spi, rotation=180,                         # 1.5" SSD1351
+# disp = ssd1351.SSD1351(spi, height=96, y_offset=32, rotation=180, # 1.27" SSD1351
+# disp = ssd1331.SSD1331(spi, rotation=180,                         # 0.96" SSD1331
+disp1 = ili9341.ILI9341(
+    spi,
+    rotation=180,  # 2.2", 2.4", 2.8", 3.2" ILI9341
+    cs=cs_pin0,
+    dc=dc_pin0,
+    rst=reset_pin0,
+    baudrate=BAUDRATE,
+)
+disp2 = ili9341.ILI9341(
+    spi,
+    rotation=180,  # 2.2", 2.4", 2.8", 3.2" ILI9341
+    cs=cs_pin1,
+    dc=dc_pin1,
+    rst=reset_pin1,
+    baudrate=BAUDRATE,
+)
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+if disp1.rotation % 180 == 90:
+    height = disp1.width  # we swap height/width to rotate it to landscape!
+    width = disp1.height
+else:
+    width = disp1.width  # we swap height/width to rotate it to landscape!
+    height = disp1.height
+
+image = Image.new("RGB", (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a green filled box as the background
+draw.rectangle((0, 0, width, height), fill=(0, 255, 0))
+disp1.image(image)
+disp2.image(image)
+
+# Draw a smaller inner purple rectangle
+draw.rectangle((BORDER, BORDER, width - BORDER - 1, height - BORDER - 1), fill=(170, 0, 136))
+
+# Load a TTF Font
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", FONTSIZE)
+
+# Draw Some Text
+text = "Screen 1"
+#(font_width, font_height) = font.getsize(text)
+(font_left, font_top, font_right, font_bottom) = font.getbbox(text)
+font_width = font_right - font_left
+font_height = font_bottom = font_top
+draw.text(
+    (width // 2 - font_width // 2, height // 2 - font_height // 2),
+    text,
+    font=font,
+    fill=(255, 255, 0),
+)
+
+# Display image.
+disp1.image(image)
+
+# Draw a smaller inner purple rectangle
+draw.rectangle((BORDER, BORDER, width - BORDER - 1, height - BORDER - 1), fill=(255, 255, 0))
+
+# Load a TTF Font
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", FONTSIZE)
+
+# Draw Some Text
+text = "Screen 2"
+#(font_width, font_height) = font.getsize(text)
+(font_left, font_top, font_right, font_bottom) = font.getbbox(text)
+font_width = font_right - font_left
+font_height = font_bottom = font_top
+draw.text(
+    (width // 2 - font_width // 2, height // 2 - font_height // 2),
+    text,
+    font=font,
+    fill=(170, 0, 136),
+)
+
+# Display image.
+disp2.image(image)
+
